c27803e46029f7204e7ead02281a0cf3f45f502d
julienlegoux <lgx.julien@gmail.com>
2022-09-14 07:17:00 +0200

    0.5 14/09/2022

---
+++ src/app/pokemon/pokemon.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {BorderCardDirective} from "./border-card.directive";
+import {PokemonTypeColorPipe} from "./pokemon-type-color.pipe";
+import {ListPokemonComponent} from "./list-pokemon/list-pokemon.component";
+import {DetailPokemonComponent} from "./detail-pokemon/detail-pokemon.component";
+import {RouterModule, Routes} from "@angular/router";
+import {PokemonService} from "./pokemon.service";
+
+const pokemonRoutes: Routes = [
+  { path: 'pokemons', component: ListPokemonComponent },
+  { path: 'pokemon/:id', component: DetailPokemonComponent }
+];
+
+@NgModule({
+  declarations: [
+    BorderCardDirective,
+    PokemonTypeColorPipe,
+    ListPokemonComponent,
+    DetailPokemonComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(pokemonRoutes)
+  ],
+  providers: [PokemonService]
+})
+export class PokemonModule { }
